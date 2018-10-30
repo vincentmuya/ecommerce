ba134495efc537315f1aa5d48ffdb8b78f3db830
vincentmuya <vincentmuya13@gmail.com>
2018-10-30 23:15:32 +0300

    created 1st model and installed tinymce

--- project/app/models.py
+++ project/app/models.py
@@ -1,3 +1,9 @@
 from django.db import models
+from django.contrib.auth.models import User
+from tinymce.models import HTMLField
 
 # Create your models here.
+class Item(models.Model):
+    item_name = models.CharField(max_length= 100)
+    item_image = models.ImageField(upload_to="posts/",blank = True, null = True)
+    item_description = HTMLField()

0f4fe8a38fced1196545b6c5bac6aa5e16edc687
vincentmuya <vincentmuya13@gmail.com>
2018-12-04 21:46:13 +0300

    modified views category function

--- project/app/views.py
+++ project/app/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, get_object_or_404, redirect
 from django.http  import HttpResponse
 from .models import Item,Category
 from .forms import NewItemForm

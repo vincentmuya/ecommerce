0e9c6abb4641dfaa3863260124967489b16e05e8
vincentmuya <vincentmuya13@gmail.com>
2018-12-05 01:07:21 +0300

    modified url and added item_details template. Modifing the view function to pick item by pk

--- project/app/urls.py
+++ project/app/urls.py
@@ -7,7 +7,7 @@ urlpatterns=[
     url(r'^$',views.index,name ='index'),
     url(r'^new/item$', views.new_item, name='new-item'),
     url(r'^search/', views.search_results, name='search_results'),
-    url('item/<int:pk>/', views.item_detail, name='item_detail'),
+    url('item/(?P<pk>\d+)/$', views.item_detail, name='item_detail'),
     url(r'^category/(?P<pk>\d+)/$', views.category_items, name='category_items'),
 ]
 

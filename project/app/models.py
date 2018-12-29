46e52767a1b0f8192b66b64ea83b2cc1c0bd8ef9
vincentmuya <vincentmuya13@gmail.com>
2018-12-29 13:37:18 +0300

    added profile object and profile dependencies

--- project/app/models.py
+++ project/app/models.py
@@ -2,6 +2,8 @@ from django.db import models
 from django.contrib.auth.models import User
 from tinymce.models import HTMLField
 from django.utils import timezone
+from django.db.models.signals import post_save
+from django.dispatch import receiver
 
 # Create your models here.
 class Category(models.Model):
@@ -11,6 +13,8 @@ class Category(models.Model):
 
 class Item(models.Model):
     seller = models.ForeignKey(User, null = True)
+    seller_number = models.IntegerField(null= True)
+    seller_location = models.CharField(null = True,max_length= 100)
     item_name = models.CharField(max_length= 100)
     category = models.ForeignKey(Category, null =True)
     item_price = models.IntegerField(null=True)
@@ -30,3 +34,23 @@ class Item(models.Model):
     def search_by_item_name(cls,search_term):
         search_result = cls.objects.filter(item_name__icontains=search_term)
         return search_result
+
+class Profile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    profile_image = models.ImageField(upload_to="posts/",blank = True, null = True)
+    seller_number = models.IntegerField(null= True)
+    seller_location = models.CharField(null = True,max_length= 100)
+
+    def save_profile(self):
+        self.save()
+
+    @classmethod
+    def this_profile(cls):
+        profile = cls.objects.all()
+        return profile
+
+def Create_profile(sender, **kwargs):
+    if kwargs['created']:
+        user_profile = Profile.objects.create(user=kwargs['instance'])
+
+post_save.connect(Create_profile,sender=User)

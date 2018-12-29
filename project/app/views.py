d2f1f10ec9250748698220e4eadd82b93c18fe49
vincentmuya <vincentmuya13@gmail.com>
2018-12-29 14:50:02 +0300

    droped database and make migrations with new changes

--- project/app/views.py
+++ project/app/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render, get_object_or_404, redirect
 from django.http  import HttpResponse
 from .models import Item,Category,Profile
-from .forms import NewItemForm
+from .forms import NewItemForm,UserForm,ProfileForm
 from django.contrib.auth.decorators import login_required
 from django.utils import timezone
 from django.db import transaction

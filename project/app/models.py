531ba6abb9d57399d5a35edd3214e8c957544c9f
vincentmuya <vincentmuya13@gmail.com>
2019-01-09 18:04:10 +0300

    modifiedprofile template

--- project/app/models.py
+++ project/app/models.py
@@ -38,11 +38,14 @@ class Item(models.Model):
 class Profile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
     profile_image = models.ImageField(upload_to="profile_pic/",blank = True, null = True)
-    seller_number = models.IntegerField(null= True)
-    seller_location = models.CharField(null = True,max_length= 100)
+    seller_number = models.IntegerField(blank = True )
+    seller_location = models.CharField(blank = True,max_length= 100)
 
     def save_profile(self):
         self.save()
+
+    # def __str__(self):
+    #     return self.user
 
     @classmethod
     def this_profile(cls):

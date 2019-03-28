ff30c4364c5a99f7e70907192b623ffd35c27a59
vincentmuya <vincentmuya13@gmail.com>
2019-03-28 23:38:30 +0300

    modified file structue for deployment and ready to push to heroku

--- project/settings.py
+++ project/settings.py
@@ -22,10 +22,10 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/1.11/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'a6l@9g(wg-@-oxu-b6%0nzt@@_w!abi!cv*i!v@ksclwzrsrfm'
+SECRET_KEY = config('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = config('DEBUG', default=False, cast=bool)
 
 ALLOWED_HOSTS = []
 

dac009139111c31081383dae6791a2615279c83a
vincentmuya <vincentmuya13@gmail.com>
2019-06-04 00:43:35 +0300

    Recoded classes in models and modified code

--- app/models.py
+++ app/models.py
@@ -7,32 +7,43 @@ from django.dispatch import receiver
 
 # Create your models here.
 class Category(models.Model):
-    categories = models.CharField(max_length= 100,null= True)
-    def __str__(self):
-        return self.categories
+    name = models.CharField(max_length=200,
+                            db_index=True)
+    slug = models.SlugField(max_length=200,
+                            db_index=True,
+                            unique=True)
+
+    class Meta:
+        ordering = ('name',)
+        verbose_name = 'category'
+        verbose_name_plural = 'categories'
+
+        def __str__(self):
+            return self.name
+
 
 class Item(models.Model):
-    seller = models.ForeignKey(User, null = True)
-    seller_number = models.IntegerField(null= True)
-    seller_location = models.CharField(null = True,max_length= 100)
-    item_name = models.CharField(max_length= 100)
-    category = models.ForeignKey(Category, null =True)
-    item_price = models.IntegerField(null=True)
-    item_image = models.ImageField(upload_to="posts/",blank = True, null = True)
-    item_description = models.TextField(max_length = 1000)
-    created_date = models.DateTimeField(default=timezone.now)
-    published_date = models.DateTimeField(blank=True, null=True)
+    category = models.ForeignKey(Category, related_name='products')
+    name = models.CharField(max_length=200, db_index=True)
+    slug = models.SlugField(max_length=200, db_index=True)
+    image = models.ImageField(upload_to="posts/",blank = True, null = True)
+    description = models.TextField(blank=True)
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+    stock = models.PositiveIntegerField()
+    available = models.BooleanField(default=True)
+    created = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now=True)
 
-    def publish(self):
-        self.published_date = timezone.now()
-        self.save()
+    class Meta:
+        ordering = ('name',)
+        index_together = (('id', 'slug'),)
 
     def __str__(self):
-        return self.item_name
+        return self.name
 
     @classmethod
-    def search_by_item_name(cls,search_term):
-        search_result = cls.objects.filter(item_name__icontains=search_term)
+    def search_by_name(cls,search_term):
+        search_result = cls.objects.filter(name__icontains=search_term)
         return search_result
 
 class Profile(models.Model):

ac3fa34724c323d1109a018195b6bd4be91cbeed
vincentmuya <vincentmuya13@gmail.com>
2019-06-05 21:46:07 +0300

    added cart detail view function

--- cart/views.py
+++ cart/views.py
@@ -19,3 +19,8 @@ def cart_remove(request, product_id):
 cart = Cart(request)
 product = get_object_or_404(Product, id=product_id)cart.remove(product)
 return redirect('cart:cart_detail')
+
+
+def cart_detail(request):
+    cart = Cart(request)
+    return render(request, 'cart/detail.html', {'cart': cart})

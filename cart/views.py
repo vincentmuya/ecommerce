ea63f51248bdea06e35a0b1e8e77dd14268de0cf
vincentmuya <vincentmuya13@gmail.com>
2019-06-05 21:43:06 +0300

    created remove to cart view function

--- cart/views.py
+++ cart/views.py
@@ -14,3 +14,8 @@ def cart_add(request, product_id):
         cd = form.cleaned_data
         cart.add(product=product, quantity=cd['quantity'], update_quantity=cd['update'])
         return redirect('cart:cart_detail')
+
+def cart_remove(request, product_id):
+cart = Cart(request)
+product = get_object_or_404(Product, id=product_id)cart.remove(product)
+return redirect('cart:cart_detail')

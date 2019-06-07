f516c22bd2674eda0bc1d7adc4e6b318e4e096ba
vincentmuya <vincentmuya13@gmail.com>
2019-06-07 21:58:17 +0300

    added category section in item_detail template

--- app/views.py
+++ app/views.py
@@ -21,8 +21,9 @@ def product_list(request, category_slug=None):
 
 def product_detail(request, id, slug):
     product = get_object_or_404 (Product, id=id, slug=slug, available=True)
+    categories = Category.objects.all()
     cart_product_form = CartAddProductForm()
-    return render(request, 'item_details.html',{"product":product,'cart_product_form': cart_product_form})
+    return render(request, 'item_details.html',{"product":product,'cart_product_form': cart_product_form,'categories': categories})
 
 @login_required(login_url='/accounts/login')
 def new_item(request):

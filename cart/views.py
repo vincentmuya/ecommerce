82b58c28005e0e543cdb6e594ab2a3b3574073bf
vincentmuya <vincentmuya13@gmail.com>
2019-06-07 21:23:54 +0300

    modified views,detail templates of the cart application

--- cart/views.py
+++ cart/views.py
@@ -1,8 +1,10 @@
 from django.shortcuts import render, redirect, get_object_or_404
+from django.http  import HttpResponse
 from django.views.decorators.http import require_POST
 from app.models import Product
 from .cart import Cart
 from .forms import CartAddProductForm
+from django.urls import reverse
 
 @require_POST
 def cart_add(request, product_id):
@@ -12,14 +14,14 @@ def cart_add(request, product_id):
     if form.is_valid():
         cd = form.cleaned_data
         cart.add(product=product,quantity=cd['quantity'],update_quantity=cd['update'])
-    return redirect('cart_detail')
+    return redirect(reverse('cart_detail'))
 
 def cart_remove(request, product_id):
     cart = Cart(request)
     product = get_object_or_404(Product, id=product_id)
     cart.remove(product)
-    return redirect('cart_detail')
+    return redirect('cart_detail' ,permanent=True)
 
 def cart_detail(request):
     cart = Cart(request)
-    return render(request, 'cart/detail.html', {'cart': cart})
+    return render(request, 'detail.html', {'cart': cart})

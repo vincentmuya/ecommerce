cc5a503cbf6891dbbb1e5f69fdd97ec02bf37973
vincentmuya <vincentmuya13@gmail.com>
2019-06-12 00:41:11 +0300

    modified code after upgrading but still redirecting to cart raises "302" then 404

--- app/views.py
+++ app/views.py
@@ -16,14 +16,8 @@ def product_list(request, category_slug=None):
     products = Product.objects.filter(available=True)
     if category_slug:
         category = get_object_or_404(Category, slug=category_slug)
-        products = Product.objects.filter(category=category)
-
-    context = {
-        'category': category,
-        'categories': categories,
-        'products': products
-    }
-    return render(request, 'index.html', context)
+        products = products.filter(category=category)
+    return render(request, 'index.html',{'category': category,'categories': categories, 'products': products})
 
 def product_detail(request, id, slug):
     product = get_object_or_404 (Product, id=id, slug=slug, available=True)

5668cc2b9669e4a514eb55fc6cbf479c96923359
vincentmuya <vincentmuya13@gmail.com>
2019-06-11 23:33:17 +0300

    modifed add_product function in app views

--- app/views.py
+++ app/views.py
@@ -16,9 +16,15 @@ def product_list(request, category_slug=None):
     products = Product.objects.filter(available=True)
     if category_slug:
         category = get_object_or_404(Category, slug=category_slug)
-        products = products.filter(category=category)
-    return render(request, 'index.html',{'category': category,'categories': categories, 'products': products})
+        products = Product.objects.filter(category=category)
 
+    context = {
+        'category': category,
+        'categories': categories,
+        'products': products
+    }
+    return render(request, 'shop/product/list.html', context)
+    
 def product_detail(request, id, slug):
     product = get_object_or_404 (Product, id=id, slug=slug, available=True)
     categories = Category.objects.all()

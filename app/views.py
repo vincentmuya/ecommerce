c3ac06c64aeec95e675b058597766a541125609f
vincentmuya <vincentmuya13@gmail.com>
2019-06-11 23:42:45 +0300

    modified both views but no reverse match or either page not found(says cause is from the product_list of app)

--- app/views.py
+++ app/views.py
@@ -23,8 +23,8 @@ def product_list(request, category_slug=None):
         'categories': categories,
         'products': products
     }
-    return render(request, 'shop/product/list.html', context)
-    
+    return render(request, 'index.html', context)
+
 def product_detail(request, id, slug):
     product = get_object_or_404 (Product, id=id, slug=slug, available=True)
     categories = Category.objects.all()

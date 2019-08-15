2d1d9f4633156cbeb96a8ab71dd77d767551375b
vincentmuya <vincentmuya13@gmail.com>
2019-08-16 01:33:11 +0300

    added login required to add_cart function

--- cart/views.py
+++ cart/views.py
@@ -3,7 +3,10 @@ from django.views.decorators.http import require_POST
 from app.models import Product
 from .cart import Cart
 from .forms import CartAddProductForm
+from django.contrib.auth.decorators import login_required
 
+
+@login_required(login_url='/accounts/login')
 @require_POST
 def cart_add(request, product_id):
     cart = Cart(request)

98ba489f78a29347086eb6ef1a0af451432cb5de
vincentmuya <vincentmuya13@gmail.com>
2019-09-07 23:16:46 +0300

    fixed attribute error in category template

--- app/views.py
+++ app/views.py
@@ -13,7 +13,7 @@ from cart.forms import CartAddProductForm
 def product_list(request, category_slug=None):
     category = None
     categories = Category.objects.all()
-    products = Product.objects.filter(available=True)[::-1]
+    products = Product.objects.filter(available=True)
     if category_slug:
         category = get_object_or_404(Category, slug=category_slug)
         products = products.filter(category=category)

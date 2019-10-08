dd63243e98a6267a4175e7fe043f70de33604c9d
vincentmuya <vincentmuya13@gmail.com>
2019-10-08 14:48:45 +0300

    modified views template

--- app/views.py
+++ app/views.py
@@ -7,7 +7,30 @@ from django.utils import timezone
 from django.db import transaction
 from django.http import JsonResponse
 from cart.forms import CartAddProductForm
+import africastalking
 
+
+# Initialize SDK
+# username = "sandbox"    # use 'sandbox' for development in the test environment
+# api_key = "4bfc140965fdf936a7de2adada43977c89441a3d4af85286e4fdc2bd7b6e885f" # use your sandbox app API key for development in the test environment
+# africastalking.initialize(username, api_key)
+#
+#
+# # Initialize a service e.g. SMS
+# sms = africastalking.SMS
+#
+#
+# # Use the service synchronously
+# response = sms.send("Hello Message!", ["+2547xxxxxx"])
+# print(response)
+#
+# # Or use it asynchronously
+# def on_finish(error, response):
+#     if error is not None:
+#         raise error
+#     print(response)
+#
+# sms.send("Hello Message!", ["+2547xxxxxx"], callback=on_finish)
 # Create your views here.
 
 def product_list(request, category_slug=None):
